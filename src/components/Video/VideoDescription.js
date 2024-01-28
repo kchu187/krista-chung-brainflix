197f99ca189c2c12443c511fe4b66fd64a13bb44
Krista Chung <kchung187@gmail.com>
2024-01-28 04:58:21 -0700

    Add styling for video description

--- src/components/Video/VideoDescription.js
+++ src/components/Video/VideoDescription.js
@@ -4,28 +4,41 @@ import LikesIcon from "../../assets/images/Icons/likes.svg";
 
 const VideoDescription = ({ selectedVideoDetails }) => {
   return (
-    <section>
-      <h2> {selectedVideoDetails.title} </h2>
-      <p> By {selectedVideoDetails.channel}</p>
-      <p> {selectedVideoDetails.timestamp}</p>
-      <p>
-        <img
-          src={ViewsIcon}
-          className="video-description__sub-details"
-          alt="Icon image of an eye"
-        />
-        {selectedVideoDetails.views}
-      </p>
-      <p>
-        {" "}
-        <img
-          src={LikesIcon}
-          className="video-description__sub-details"
-          alt="Small grey thumbs-up icon"
-        />
-        {selectedVideoDetails.likes}
-      </p>
-
+    <section className="video-description">
+      <h2 className="video-description__title">{selectedVideoDetails.title}</h2>
+      <div className="video-description__container">
+        <div className="video-description__sub-container">
+          <p className="video-description__sub-description--bold">
+            {" "}
+            By {selectedVideoDetails.channel}
+          </p>
+          <p className="video-description__sub-description--grey">
+            {" "}
+            {selectedVideoDetails.timestamp}
+          </p>
+        </div>
+        <div className="video-description__sub-container">
+          <p className="video-description__sub-description--grey">
+            <img
+              className="video-description__icon"
+              src={ViewsIcon}
+              className="video-description__sub-details"
+              alt="Icon image of an eye"
+            />
+            {selectedVideoDetails.views}
+          </p>
+          <p className="video-description__sub-description--grey">
+            {" "}
+            <img
+              className="video-description__icon"
+              src={LikesIcon}
+              className="video-description__sub-details"
+              alt="Small grey thumbs-up icon"
+            />
+            {selectedVideoDetails.likes}
+          </p>
+        </div>
+      </div>
       <p> {selectedVideoDetails.description} </p>
     </section>
   );
