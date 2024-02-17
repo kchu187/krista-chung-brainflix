cf8c344224af42c5556c524b860a1f89d6a58605
Krista Chung <kchung187@gmail.com>
2024-02-17 05:02:06 -0700

    refactor VideoLIst to not include click handler and change URL instead with react router

--- src/components/Video/VideoList.js
+++ src/components/Video/VideoList.js
@@ -1,30 +1,24 @@
 import "./VideoList.scss";
+import { Link } from "react-router-dom";
 
-const VideoList = ({ videos, onSelectedVideo }) => {
-  const handleVideoClick = (video) => {
-    // Call the onSelectedVideo callback with the clicked video (edit comment)
-    onSelectedVideo(video);
-  };
-
+const VideoList = ({ videos, setSelectedVideo }) => {
   return (
     <section className="video-list__container">
       <h2 className="video-list__heading">NEXT VIDEOS</h2>
       <ul className="video-list">
         {videos.map((video) => (
-          <li
-            className="video-list__item"
-            key={video.id}
-            onClick={() => handleVideoClick(video)}
-          >
-            <img
-              className="video-list__image"
-              src={video.image}
-              alt={video.title}
-            />
-            <div className="video-list__text-container">
-              <p className="video-list__title">{video.title}</p>
-              <p className="video-list__channel">{video.channel}</p>
-            </div>
+          <li className="video-list__item" key={video.id}>
+            <Link to={`/videos/${video.id}`}>
+              <img
+                className="video-list__image"
+                src={video.image}
+                alt={video.title}
+              />
+              <div className="video-list__text-container">
+                <p className="video-list__title">{video.title}</p>
+                <p className="video-list__channel">{video.channel}</p>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import axios from "axios";
-import { API_KEY, BASE_URL } from "../../utils.js./utils";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { API_KEY, BASE_URL } from "../../utils.js";
 import "./App.scss";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from "../../components/Header/Header.js";
-import VideoData from "../../Data/videos.json";
+// import VideoData from "../../Data/videos.json";
 import VideoDetailsData from "../../Data/video-details.json";
 import VideoPlayer from "../../components/Video/VideoPlayer.js";
 import VideoDescription from "../../components/Video/VideoDescription.js";
@@ -16,7 +15,23 @@ import CommentForm from "../../components/Comments/CommentForm.js";
 
 const HomePage = () => {
   //Set state for the video that has been clicked which will be the selected video
-  const [selectedVideo, setSelectedVideo] = useState(VideoData[0]);
+  const [selectedVideo, setSelectedVideo] = useState([]);
+
+  useEffect(() => {
+    const fetchVideoData = async () => {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}/videos?api_key=${API_KEY}`
+        );
+        setSelectedVideo(response.data);
+
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error, could not fetch video data", error);
+      }
+    };
+    fetchVideoData();
+  }, []);
 
   // Set separate state for video details
   const [selectedVideoDetails, setSelectedVideoDetails] = useState(
