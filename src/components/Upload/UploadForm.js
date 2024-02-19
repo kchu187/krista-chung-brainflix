a95dd91e04d66385106db1366ee2482c53b22a25
Krista Chung <kchung187@gmail.com>
2024-02-18 19:52:09 -0700

    Create skeleton react Upload form

--- src/components/Upload/UploadForm.js
+++ src/components/Upload/UploadForm.js
@@ -1,10 +1,49 @@
 import "./UploadForm.scss";
-
+import { useState } from "react";
+import Button from "../Button.js";
+import UploadIcon from "../../assets/images/Icons/upload.svg";
 function UploadForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("Upload in progress, re-directing to homepage");
+  };
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
   return (
-    <div className="upload-form">
+    <form className="upload-form" onSubmit={handleSubmit}>
       <h1> Upload Video</h1>
-    </div>
+      <h2 className="upload-form__title">Video Thumbnail</h2>
+
+      <label>
+        TITLE YOUR VIDEO
+        <input
+          type="text"
+          name="video-title"
+          value={title}
+          onChange={handleChangeTitle}
+        />
+      </label>
+      <label>
+        ADD A VIDEO DESCRIPTION{" "}
+        <input
+          type="textarea"
+          name="video-title"
+          value="Add a description to your video"
+        />
+      </label>
+      <Button
+        buttonClassName="button__comment-form"
+        label="PUBLISH"
+        iconImage={UploadIcon}
+        iconClassName="button__icon--comment-form"
+        type="submit"
+      />
+    </form>
   );
 }
 
