e53daa08f8ee1659be754cd5eb8dbd2992c5b512
Krista Chung <kchung187@gmail.com>
2024-02-18 21:46:56 -0700

    Finish desktop styling on Upload page

--- src/components/Upload/UploadForm.js
+++ src/components/Upload/UploadForm.js
@@ -24,35 +24,41 @@ function UploadForm() {
   return (
     <form className="upload-form" onSubmit={handleSubmit}>
       <h1 className="upload-form__header"> Upload Video</h1>
-      <h2 className="upload-form__title">VIDEO THUMBNAIL</h2>
-      <img
-        className="upload-form__image"
-        src={UploadPreview}
-        alt="Bicycle handle"
-      />
+      <div className="upload-form__container">
+        <div className="upload-form__thumbnail-subcontainer">
+          <h2 className="upload-form__title">VIDEO THUMBNAIL</h2>
 
-      <label className="upload-form__title">
-        TITLE YOUR VIDEO
-        <input
-          className="upload-form__field"
-          type="text"
-          name="video-title"
-          value={title}
-          placeholder="Add a title to your video"
-          onChange={handleChangeTitle}
-        />
-      </label>
-      <label className="upload-form__title--description">
-        ADD A VIDEO DESCRIPTION
-        <input
-          className="upload-form__field--description"
-          type="text"
-          name="video-description"
-          value={description}
-          placeholder="Add a description to your video"
-          onChange={handleChangeDescription}
-        />
-      </label>
+          <img
+            className="upload-form__image"
+            src={UploadPreview}
+            alt="Bicycle handle"
+          />
+        </div>
+        <div className="upload-form__subcontainer">
+          <label className="upload-form__title">
+            TITLE YOUR VIDEO
+            <input
+              className="upload-form__field"
+              type="text"
+              name="video-title"
+              value={title}
+              placeholder="Add a title to your video"
+              onChange={handleChangeTitle}
+            />
+          </label>
+          <label className="upload-form__title--description">
+            ADD A VIDEO DESCRIPTION
+            <input
+              className="upload-form__field--description"
+              type="text"
+              name="video-description"
+              value={description}
+              placeholder="Add a description to your video"
+              onChange={handleChangeDescription}
+            />
+          </label>
+        </div>
+      </div>
       <Button
         buttonClassName="button__upload-form"
         label="PUBLISH"
