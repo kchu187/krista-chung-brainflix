d2c6ffddfac2a5bb3347e51e4ce96af8629cb90e
Krista Chung <kchung187@gmail.com>
2024-03-10 15:50:35 -0600

    Modify stylin and prevent comment button from refreshing

--- src/components/Comments/CommentForm.js
+++ src/components/Comments/CommentForm.js
@@ -3,6 +3,9 @@ import Button from "../Button.js";
 import CommentIcon from "../../assets/images/Icons/add_comment.svg";
 import Avatar from "../../assets/images/Mohan-muruge.jpg";
 const CommentForm = ({ selectedVideoDetails }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="comments-form__container">
       <img
@@ -11,7 +14,7 @@ const CommentForm = ({ selectedVideoDetails }) => {
         alt="sideprofile of a man"
       ></img>
 
-      <form className="comments-form">
+      <form className="comments-form" onSubmit={handleSubmit}>
         <h2 className="comments-form__title"> JOIN THE CONVERSATION </h2>
         <textarea
           className="comments-form__field"
@@ -24,6 +27,7 @@ const CommentForm = ({ selectedVideoDetails }) => {
           label="COMMENT"
           iconImage={CommentIcon}
           iconClassName="button__icon--comment-form"
+          type="submit"
         />
       </form>
     </div>
